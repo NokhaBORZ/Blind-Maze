b95ecdd001335d5d34813364cf53cd0f6526e20b
oltir06 <ol871rex@htwg-konstanz.de>
2026-02-02 02:41:28 +0100

    feat(items): implement Lightning item functionality
    - Implement Lightning.use() to reveal ALL walls on the grid
    - Set rarity to Rare (more powerful than Lantern)
    - Uses existing grid.showAllWalls() method

--- src/main/scala/de/htwg/se/blindmaze/model/item/ItemsImp/Lightning.scala
+++ src/main/scala/de/htwg/se/blindmaze/model/item/ItemsImp/Lightning.scala
@@ -1,9 +1,23 @@
 package de.htwg.se.blindmaze.model.item.ItemsImp
 
 import de.htwg.se.blindmaze.model.managers.IGameManager
+import de.htwg.se.blindmaze.model.managers.managersImp.RunningState
 import de.htwg.se.blindmaze.model.item.{IItem, Rarity}
 
-case class Lightning(name: String, description: String = "") extends IItem {
-    override def use(gameManager: IGameManager): IGameManager = ???
-    override def rarity: Rarity = Rarity.Common
+/**
+ * Lightning item that reveals ALL walls on the grid.
+ * A powerful rare item for strategic advantage.
+ */
+case class Lightning(name: String, description: String = "A powerful flash that reveals all walls in the maze") extends IItem {
+  
+  override def use(gameManager: IGameManager): IGameManager = {
+    gameManager match {
+      case running: RunningState =>
+        val revealedGrid = running.grid.showAllWalls()
+        running.copy(grid = revealedGrid)
+      case _ => gameManager // Not in running state, return unchanged
+    }
+  }
+  
+  override def rarity: Rarity = Rarity.Rare
 }
